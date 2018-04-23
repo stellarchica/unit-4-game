cea4a126e881d16a83c4bbd8331111bee353b06d
sarah martinez <sarahmartinez@sarahs-macbook.attlocal.net>
2018-04-23 01:41:21 -0500

    html, css base done, pseudocode of js complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var winsCounter = 0;
+
+var lossesCounter = 0;
+
+var userNumber = 0;
+
+var jewelValue = 1-10;
+
+var randomNumber = 10-50;
+
+// check logic
+randomNumber = wordsList[Math.floor(Math.random() * randomNumber.length)];
+
+
+// logic has to select random number
+// user clicks on crystal
+    // need to make onclick or as an event
+// corresponding crystal adds value to user number on click
+    // set random values for each of the crystals
+        // will need to change per game
+        // cannot be the same number e ery game
+// user repeats process until one of two events
+    // user number either matches random number
+        // then winning message is displayed
+        // tally added to win counter
+    // user number passes random number
+        // then losing message is displayed
+        // tally added to lose counter
+        
+// game will reset & change values automatically?
+    // bonus points for this, plus if without clicking reload button
+    // would have to create as a for loop?
+
