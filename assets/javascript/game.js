c974102ad710d86d93f3b1e494baddbbd2fd86d4
sarah martinez <sarahmartinez@sarahs-MacBook.local>
2018-04-24 00:03:20 -0500

    pseudocode and first draft of code, not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,101 @@
-var winsCounter = 0;
+$(document).ready(function(){ 
+    var randomNumber = [Math.floor(Math.random() * 100)];
 
-var lossesCounter = 0;
+    $('#randomNumber').text(randomNumber);
 
-var userNumber = 0;
+    var jewelOneValue = [Math.floor(Math.random()* 10)];
+    var jewelTwoValue = [Math.floor(Math.random()* 10)];
+    var jewelThreeValue = [Math.floor(Math.random()* 10)];
+    var jewelFourValue = [Math.floor(Math.random()* 10)];
 
-var jewelValue = 1-10;
+    var winsCounter = 0;
+    var lossesCounter = 0;
+    var userNumber = 0;
 
-var randomNumber = 10-50;
+    $('#numberWins').text(winsCounter);
+    $('#numberLosses').text(lossesCounter);
 
-// check logic
-randomNumber = wordsList[Math.floor(Math.random() * randomNumber.length)];
+    function reset(){
+        randomNumber=Math.floor(Math.random()*101+19);
+        console.log(randomNumber)
+        $('#randomNumber').text(randomNumber);
+        jewelOneValue = [Math.floor(Math.random()* 10)];
+        jewelTwoValue = [Math.floor(Math.random()* 10)];
+        jewelThreeValue = [Math.floor(Math.random()* 10)];
+        jewelFourValue = [Math.floor(Math.random()* 10)];
+        userNumber= 0;
+        // create a variable for total number?
+        $('#totalNumber').text(userNumber);
+        }
 
+        // convert into if-then statements?
+    function success(){
+        alert("You won!");
+            wins++; 
+            $('#numberWins').text(winsCounter);
+            reset();
+        }
 
-// logic has to select random number
+    function failure(){
+        alert ("You lose!");
+            losses++;
+            $('#numberLosses').text(lossesCounter);
+            reset()
+        }
+
+    $('#jewel-one').on ('click', function(){
+        userNumber = userNumber + jewelOneValue;
+        console.log("New userNumber= " + userNumber);
+        $('#finalTotal').text(userNumber); 
+            //sets win/lose conditions
+            if (userNumber == randomNumber){
+            success();
+            }
+            else if ( userNumber > randomNumber){
+            failure();
+            }   
+    })  
+    $('#jewel-two').on ('click', function(){
+        userNumber = userNumber + jewelTwoValue;
+        console.log("New userNumber= " + userNumber);
+        $('#finalTotal').text(userNumber); 
+            if (userNumber == randomNumber){
+            success();
+            }
+            else if ( userNumber > randomNumber){
+            failure();
+            } 
+    })  
+    $('#jewel-three').on ('click', function(){
+        userNumber = userNumber + jewelThreeValue;
+        console.log("New userNumber= " + userNumber);
+        $('#finalTotal').text(userNumber);
+    //sets win/lose conditions
+            if (userNumber == randomNumber){
+            success();
+            }
+            else if ( userNumber > randomNumber){
+            failure();
+            } 
+    })  
+    $('#jewel-four').on ('click', function(){
+        userNumber = userNumber + jewelFourValue;
+        console.log("New userNumber= " + userNumber);
+        $('#finalTotal').text(userNumber); 
+        
+            if (userNumber == randomNumber){
+            success();
+            }
+            else if ( userNumber > randomNumber){
+            failure();
+            }
+    });   
+}); 
+
+// USE CONSOLE LOG TO MAKE SURE SHIT IS WORKING
+// fix code by comparing to logic
+
+// logic has to select random number for random number and jewel values
 // user clicks on crystal
     // need to make onclick or as an event
 // corresponding crystal adds value to user number on click
@@ -30,4 +113,4 @@ randomNumber = wordsList[Math.floor(Math.random() * randomNumber.length)];
 // game will reset & change values automatically?
     // bonus points for this, plus if without clicking reload button
     // would have to create as a for loop?
-
+// research updateDisplay
